a7f3aaa4df535bdd8272be5250ccabdb31ca2771
tommy-pettersson <tommypettersson04@gmail.com>
2021-09-30 12:35:12 +0200

    change speed of main game loop

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ screen.onkey(snake.right, "Right")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.07)
 
     if snake.is_alive:
         snake.move()
